d06145e12d79480e9edb884903fd84087a2fdd09
kibwashere <kibtpwd@gmail.com>
2024-03-30 23:26:54 +0100

    v2 plz

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-module.exports = function wls () {
-    return { getSsItem, getLsItem, setSsItem, setLsItem }
+export function getSsItem(key) {
+    return getStorageItem(key, 'sessionStorage');
+}
 
-    function getSsItem (key) {
-        return getStorageItem(key, 'sessionStorage')
+export function getLsItem(key) {
+    return getStorageItem(key, 'localStorage');
+}
+
+export function setSsItem(key, data) {
+    return setStorageItem(key, data, 'sessionStorage');
+}
+
+export function setLsItem(key, data) {
+    return setStorageItem(key, data, 'localStorage');
+}
+
+function getStorageItem(key, type) {
+    const storage = window[type];
+    const data = storage && storage.getItem(key);
+
+    try {
+        return JSON.parse(data);
+    } catch (e) {
+        return data;
     }
+}
 
-    function getLsItem (key) {
-        return getStorageItem(key, 'localStorage')
-    }
+function setStorageItem(key, data, type) {
+    const storage = window[type];
 
-    function setSsItem (key, data) {
-        return setStorageItem(key, data, 'sessionStorage')
-    }
+    try {
+        storage.setItem(key, typeof data === 'string' ? data : JSON.stringify(data));
 
-    function setLsItem (key, data) {
-        return setStorageItem(key, data, 'localStorage')
-    }
-
-    function getStorageItem (key, type) {
-        const storage = window[type]
-        const data = storage && storage.getItem(key)
-
-        try {
-            return JSON.parse(data)
-        } catch (e) {
-            return data
-        }
-    }
-
-    function setStorageItem (key, data, type) {
-        const storage = window[type]
-        
-        try {
-            storage.setItem(key, typeof data === 'string' ? data : JSON.stringify(data))
-
-            return true
-        } catch (e) {
-            return false
-        }
+        return true;
+    } catch (e) {
+        return false;
     }
 }
